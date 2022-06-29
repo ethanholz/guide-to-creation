c076aa8847b3adf802c11f12ee74ca241e89eb41
Ethan Holz <iplaybassx2@gmail.com>
2022-06-29 12:24:57 -0500

    Python updates, missing distutils

--- ansible-init.sh
+++ ansible-init.sh
@@ -5,19 +5,24 @@ then
     echo "python not found please install"
     exit
 fi
+sudo apt-get install python3-distutils -y
 if ! command -v curl &> /dev/null
 then
-    echo "curl not found please install"
-    exit
+    echo "curl not found... attempting install"
+    sudo apt-get install curl -y
 fi
 if ! command -v pip &> /dev/null
 then
-    echo "pip not found... installing...\n"
+    echo "pip not found... installing..."
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
     python3 get-pip.py --user
     rm get-pip.py
 fi
 
+if ! command -v pip &> /dev/null
+then
+    echo "Installation of pip failed"
+fi
 python3 -m pip install --user ansible
 if ! command -v ansible &> /dev/null
 then
